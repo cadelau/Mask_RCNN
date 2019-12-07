56eb46370db50e16a04f775a44e2a8d287e4544f
Peter Fitchen <peterfitchen@0587360558.wireless.umich.net>
2019-12-07 13:44:38 -0500

    MOD: added to rob_test.py based on initial demo colab

--- mrcnn/rob_test.py
+++ mrcnn/rob_test.py
@@ -17,3 +17,26 @@ model_path = model.find_last()
 # Load trained weights
 print("Loading weights from ", model_path)
 model.load_weights(model_path, by_name=True)
+
+
+# Test on a random image
+image_id = random.choice(dataset_test.image_ids)
+original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+    modellib.load_image_gt(dataset_test, inference_config, 
+                           image_id, use_mini_mask=False)
+
+log("original_image", original_image)
+log("image_meta", image_meta)
+log("gt_class_id", gt_class_id)
+log("gt_bbox", gt_bbox)
+log("gt_mask", gt_mask)
+
+visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, 
+                            dataset_train.class_names, figsize=(8, 8))
+
+
+results = model.detect([original_image], verbose=1)
+
+r = results[0]
+visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'], 
+                            dataset_test.class_names, r['scores'], ax=get_ax())
